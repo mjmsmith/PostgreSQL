542d39f761cd6f9034a079ba49341994317ae885
Mark Smith <mark@camazotz.com>
2016-09-17 16:03:33 -0400

    Use v1.0.0 of SQL.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
 	name: "PostgreSQL",
 	dependencies: [
 		.Package(url: "https://github.com/Zewo/CLibpq.git", majorVersion: 0, minor: 5),
-		.Package(url: "https://github.com/mjmsmith/SQL.git", majorVersion: 0, minor: 0),
+		.Package(url: "https://github.com/mjmsmith/SQL.git", majorVersion: 1)
 	]
 )
